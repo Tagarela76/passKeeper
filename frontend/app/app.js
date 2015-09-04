674d4f09e968bf57bb3291810e14ca215aedf51b
Tagarela76 <cl.denis.kv@gmail.com>
2015-09-04 18:18:43 +0300

    add menu

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,11 +1,25 @@
 'use strict';
-angular.module('passKeeperApp', ['ngRoute'])
+var passKeeperApp = angular.module('passKeeperApp', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/home', {template: 'views/home.html', controller: HomeCtrl})
-            .when('/register', {template: 'views/register.html', controller: RegisterCtrl})
-            .when('/login', {template: 'views/login.html', controller: LoginCtrl})
+            .when('/home', {templateUrl: 'app/views/home.html', controller: HomeCtrl})
+            .when('/register', {templateUrl: 'app/views/register.html', controller: RegisterCtrl})
+            .when('/login', {templateUrl: 'app/views/login.html', controller: LoginCtrl})
             //.when('/edit/:id', {template: 'views/edit.html', controller: EditCtrl})
-            .otherwise({redirectTo: '/list'});
+            .otherwise({redirectTo: '/home'});
     },
     ]);
+
+
+passKeeperApp.controller('menuCtrl', ['$scope', '$location',
+    function ($scope, $location) {
+        $scope.isActive = function(path){
+            //console.log(location.path());
+            if ($location.path().substr(0, path.length) === path) {
+                return 'active';
+            } else {
+                return '';
+            }
+
+        }
+    }]);
